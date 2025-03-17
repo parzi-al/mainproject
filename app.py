28a334f12fe427e9373e486733452bb3e46d3b59
Bhavan S <109290304+parzi-al@users.noreply.github.com>
2025-03-17 10:06:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ EXIT_CAPACITY = {
 CONGESTION_THRESHOLD = 5  # Maximum allowed users per node
 
 FIXED_ROUTERS = {
-    "GNXS-2.4G-095C60": (4, 11),
-    "GNXS-5G-095C60": (1, 8),
+    "Nandhu": (4, 11),
+    "XRGDELL 2357": (1, 8),
     "MITS_STAFF": (8, 5)
 }
 
